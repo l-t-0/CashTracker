9808fb3859c84cc0a6a170fa852969d4191b0566
l-t-0 <x22151249@student.ncirl.ie>
2023-08-06 16:53:56 +0100

    Update globalContext.js

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react'
 import axios from 'axios'
 
-const BASE_URL = 'https://cashtracker.onrender.com/';
+const BASE_URL = 'https://cashtracker.onrender.com/api/v1/';
 
 
 const GlobalContext = React.createContext()
