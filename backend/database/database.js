0e32ba8f4954e7acf6785c0d9a0bbd4f7b46643a
l-t-0 <x22151249@student.ncirl.ie>
2023-08-06 15:51:37 +0100

    uri

--- backend/database/database.js
+++ backend/database/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const database = async () => {
     try {
         mongoose.set('strictQuery', false)
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URI)
         console.log('Database connected successfully!')
     } catch (error) {
         console.log('Database connection error!');
